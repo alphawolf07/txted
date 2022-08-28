f1c2185eea0e432579e7fce440ea0809199f14b3
unknown <mandalaryapriyo@gmail.com>
2022-08-29 02:19:28 +0530

    first

---
+++ src/edu/gatech/seclass/txted/Main.java
@@ -0,0 +1,15 @@
+package edu.gatech.seclass.txted;
+
+public class Main {
+
+    // Empty Main class for compiling Individual Project.
+    // During Deliverable 1 and Deliverable 2, DO NOT ALTER THIS CLASS or implement it
+
+    public static void main(String[] args) {
+        // Empty Skeleton Method
+    }
+
+    private static void usage() {
+        System.err.println("Usage: txted [ -f | -i | -s integer | -e string | -r | -x string | -n integer ] FILE");
+    }
+}
